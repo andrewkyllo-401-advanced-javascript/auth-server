3cd2a3b041efe44c101e063778b78b9cb026a181
Andrew Kyllo <kyllo34@gmail.com>
2020-02-25 15:55:28 -0800

    user schema set up

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { MONGODB_URI, PORT } = process.env;
 // connect to mongo
 mongoose.connect(MONGODB_URI, mongooseOptions, () => {
   console.log('Connected to MongoDB.');
-})
+});
 
 // start express server
 const server = require('./src/app');
--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ const express = require('express');
 // App-level middleware
 const app = express();
 app.use(express.json());
+const basicAuth = require('./middleware/basic-auth');
+
+// models
+const User = require('./models/user');
+
+// Routes
+app.post('/signup', async (req, res) => {
+  const newUser = new User(req.body);
+});
+
+
+
+
+
+
+
+app.get('/this_will_error', (req, res) => {
+  throw new Error('Internal server error.');
+});
+
 
 // exports the server
 let isRunning = false;
@@ -16,9 +36,9 @@ module.exports = {
       app.listen(port, () => {
         isRunning = true;
         console.log(`Server is listening on port ${port}`);
-      })
+      });
     } else {
       console.error('Server is already running!')
     }
-  }
-}
+  },
+};
